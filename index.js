6e27a4d3074d165d01c4e49ea43317d03b0fcd27
DDavide-hub <davoliodavide@gmail.com>
2020-12-05 08:48:56 +0100

    corretto porta 8000

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict"
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 8000;
 
 //Loads the handlebars module
 const handlebars = require("express-handlebars");
